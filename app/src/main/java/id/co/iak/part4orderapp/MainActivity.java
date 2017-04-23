3182659a44f222590635cb0a3611228facf1c806
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-23 10:53:57 +0700

    [Add] increment and decrement functional

--- app/src/main/java/id/co/iak/part4orderapp/MainActivity.java
+++ app/src/main/java/id/co/iak/part4orderapp/MainActivity.java
@@ -1,13 +1,68 @@
 package id.co.iak.part4orderapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Views
+    private TextView txtQuantity;
+    private Button btnIncrement;
+    private Button btnDecrement;
+
+    // Data
+    private int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    /**
+     * This method initiate the views
+     */
+    private void initView() {
+        txtQuantity = (TextView) findViewById(R.id.txt_quantity);
+        btnIncrement = (Button) findViewById(R.id.btn_add);
+        btnDecrement = (Button) findViewById(R.id.btn_substract);
+
+        txtQuantity.setText(Integer.toString(quantity));
+    }
+
+    /**
+     * This method increment the Quantity text
+     *
+     * @param v is to binding the views
+     */
+    public void increment(View v) {
+        quantity++;
+        display(Integer.toString(quantity));
+    }
+
+    /**
+     * This method decrement the Quantity text
+     *
+     * @param v is to binding the views
+     */
+    public void decrement(View v) {
+        quantity--;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        display(Integer.toString(quantity));
+    }
+
+    /**
+     * This method display the output messages
+     * @param message the string to output
+     */
+    private void display(String message) {
+        txtQuantity.setText(message);
     }
 }
